e906df1c1cfbef3eb7ead3c6d715275657fd4286
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-03-06 13:29:41 +0100

    create useful methods for shell generation

--- utils.py
+++ utils.py
@@ -5,7 +5,28 @@ from copy import deepcopy
 from distributions import Gaussian
 
 
-def create_uniform_grid(lower: torch.Tensor, upper: torch.Tensor, n: int):
+def get_shell(samples: torch.Tensor):
+
+    min_point = torch.min(samples, dim=0).values
+    max_point = torch.max(samples, dim=0).values
+
+    return torch.stack((min_point, max_point), dim=0)
+
+def get_shell_loc(macro_region: torch.Tensor):
+    dimensions = macro_region.size(1)
+
+    # Calculate max and min coordinates along each dimension
+    max_coords, _ = torch.max(macro_region, dim=0)
+    min_coords, _ = torch.min(macro_region, dim=0)
+
+    # Choose a point close to an arbitrary face
+    outer_point = torch.where(torch.arange(dimensions) != dimensions - 1, (min_coords + max_coords) / 2, max_coords + 1e-1)
+    return outer_point
+
+
+def uniform_grid(lower: torch.Tensor,
+                 upper: torch.Tensor,
+                 n: int):
 
     d = lower.shape[0]
     equidistant_spaces = [torch.linspace(lower[i], upper[i], steps=n) for i in range(d)]
