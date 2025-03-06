c456d8ed15e25fd5c429ebce494ba03e5d2afc6d
adrienbanse <adrien.banse@gmail.com>
2025-03-06 17:48:18 +0100

    sup/inf analytical implementation except outer shell

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import itertools
 from copy import deepcopy
 
 from distributions import Gaussian
+from regions import HyperRectangle, Polytope
 
 
 def get_shell(samples: torch.Tensor):
@@ -36,54 +37,43 @@ def uniform_grid(lower: torch.Tensor,
     return grid
 
 
-def generate_hypercube_vertices(lower_point, upper_point):
-    d = lower_point.shape[0]  # Number of dimensions
-    vertices = torch.tensor(list(itertools.product(*zip(lower_point, upper_point))))
-    return vertices
-
-def compute_sup_inf_kernel(f, covariance, regions, set, N = 10000, low = -20, high=20):
-    regions_to = deepcopy(regions)
-
+def compute_inf_sup_kernel(f, covariance, regions, set):
     sup_probs_regions, inf_probs_regions = [], []
     sup_probs_set, inf_probs_set = [], []
 
-    for lower_point_from, upper_point_from in zip(regions.lower, regions.upper):
-
+    # TODO: suboptimal to create new objects in a for loop (HyperRectangle and Polytope)
+    for lower_from, upper_from in zip(regions.lower[:-1], regions.upper[:-1]):
         sup_prob_regions, sup_prob_set = torch.zeros(len(regions.lower)), torch.zeros(1)
         inf_prob_regions, inf_prob_set = torch.ones(len(regions.lower)), torch.ones(1)
 
-        lower_sample_from = deepcopy(lower_point_from)
-        lower_sample_from[torch.isinf(lower_sample_from)] = low
-        upper_sample_from = deepcopy(upper_point_from)
-        upper_sample_from[torch.isinf(upper_sample_from)] = high
-        samples = torch.distributions.uniform.Uniform(lower_sample_from, upper_sample_from).sample([N]) 
+        region_from = HyperRectangle(lower_from, upper_from)
+        vertices_from = region_from.get_vertices()
+        vertices_image = f(vertices_from)
+        image = Polytope(vertices_image)
+
+        for i, (lower_to, upper_to, center_to) in enumerate(zip(regions.lower[:-1], regions.upper[:-1], regions.center[:-1])):
+            region_to = HyperRectangle(lower_to, upper_to)
+            furthest, closest = image.furthest_and_closest_from(center_to)
+            kernel_inf = Gaussian(f(furthest), covariance) 
+            kernel_sup = Gaussian(f(closest), covariance) 
+            inf_prob_regions[i] = kernel_inf.compute_probabilities(region_to)
+            sup_prob_regions[i] = kernel_sup.compute_probabilities(region_to) 
         
-        for (i, (lower_point_to, upper_point_to)) in enumerate(zip(regions_to.lower, regions_to.upper)):
-            # take higher for unbounded
-            lower_point_to[torch.isinf(lower_point_to)] = 2 * low
-            upper_point_to[torch.isinf(upper_point_to)] = 2 * high  
-
-        for sample in samples:
-            kernel = Gaussian(f(sample), covariance) 
-            prob_regions = kernel.compute_probabilities(regions_to)
-            prob_set = kernel.compute_probabilities(set)
-            sup_prob_regions = torch.maximum(sup_prob_regions, prob_regions)
-            inf_prob_regions = torch.minimum(inf_prob_regions, prob_regions)
-            sup_prob_set = torch.maximum(sup_prob_set, prob_set)
-            inf_prob_set = torch.minimum(inf_prob_set, prob_set)
-
-        sup_probs_regions.append(sup_prob_regions)
+        # TODO: to the outer shell
+    
         inf_probs_regions.append(inf_prob_regions)
-
-        sup_probs_set.append(sup_prob_set)
-        inf_probs_set.append(inf_prob_set)
-
-    sup_probs_regions = torch.stack(sup_probs_regions)
+        sup_probs_regions.append(sup_prob_regions)
+    
+    # TODO: from the outer shell
+    inf_probs_regions.append(torch.zeros(len(regions.lower)))
+    # sup_probs_regions.append() # TODO sup from 
+    
     inf_probs_regions = torch.stack(inf_probs_regions)
-    sup_probs_set = torch.tensor(sup_probs_set)
+    sup_probs_regions = torch.stack(sup_probs_regions)
     inf_probs_set = torch.tensor(inf_probs_set)
+    sup_probs_set = torch.tensor(sup_probs_set)
 
-    return sup_probs_regions, inf_probs_regions, sup_probs_set, inf_probs_set
+    return inf_probs_regions, sup_probs_regions, inf_probs_set, sup_probs_set
 
 def compute_kernel_at_locs(f, locs, covariance, regions, set):
 
