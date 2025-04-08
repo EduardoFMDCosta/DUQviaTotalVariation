55d574f493b29a93aef34c3814e8a5f019f8f767
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-04-08 23:32:50 +0200

    change sup inf methods

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ from copy import deepcopy
 from distributions import Gaussian
 from dynamics import Dynamics
 from optimization import gradient_descent, project_to_closest_face
+from probabilities import kernel_probs_given_optimal_means, gaussian_probabilities
 from regions import HyperRectangle, Polytope, HyperRectangularPartition
 import bound_propagation as bp
-import torch.distributions as dist
 
 factory = bp.BoundModelFactory()
 
@@ -26,7 +26,7 @@ def get_shell_loc(macro_region: torch.Tensor):
     min_coords, _ = torch.min(macro_region, dim=0)
 
     # Choose a point close to an arbitrary face
-    outer_point = torch.where(torch.arange(dimensions) != dimensions - 1, (min_coords + max_coords) / 2, max_coords + 1e-1)
+    outer_point = torch.where(torch.arange(dimensions) != dimensions - 1, (min_coords + max_coords) / 2, max_coords + 5.0)
     return outer_point
 
 
@@ -41,57 +41,80 @@ def uniform_grid(lower: torch.Tensor,
 
     return grid
 
-
 def bound_transition_kernel(f: Dynamics,
                             partition: HyperRectangularPartition,
                             target: Union[HyperRectangularPartition, HyperRectangle],
                             covariance: torch.Tensor,
                             supremum: bool = True):
 
-    # Instantiate Normal distribution
-    normal = dist.Normal(0, 1)
-    std_devs = torch.sqrt(torch.diag(covariance))
+    # Get parameters
+    shell = partition.shell
+    target_is_partition = isinstance(target, HyperRectangularPartition)
 
-    # Create hypercubic envelope for f(R_from), denoted \barR_from
+    # Replace the unbounded by the shell (computations are made accordingly later)
+    from_lower = partition.lower
+    from_lower[-1] = shell.lower
+    from_upper = partition.upper
+    from_upper[-1] = shell.upper
+
+    # If target is partition, we replace the unbounded by the shell (computations are made accordingly later)
+    target_lower = target.lower
+    target_upper = target.upper
+    target_center = target.center
+    if target_is_partition:
+        target_lower[-1] = shell.lower
+        target_upper[-1] = shell.upper
+        target_center[-1] = shell.center
+
+    # Create hyper-cubic envelope for f(R_from)
     net = factory.build(f)
-    ibp = net.ibp(bp.HyperRectangle(partition.lower, partition.upper))
+    ibp = net.ibp(bp.HyperRectangle(from_lower, from_upper))
     from_lower = ibp.lower
     from_upper = ibp.upper
 
-    # Get centers of target
-    target_centers = target.center.unsqueeze(0) if target.center.dim() == 1 else target.center
-    target_lower = target.lower.unsqueeze(0) if target.center.dim() == 1 else target.lower
-    target_upper = target.upper.unsqueeze(0) if target.center.dim() == 1 else target.upper
+    # Project center (target_center) to cube (from_set)
+    projection = torch.clamp(target_center.unsqueeze(0),
+                            min=from_lower.unsqueeze(1),
+                            max=from_upper.unsqueeze(1))
 
-    # Compute distances from R_target centers to lower / upper of R_from
+    # Get farther point from from_set to shell
+    farthest_from_shell = torch.where(torch.abs(from_lower - shell.center) > torch.abs(from_upper - shell.center),
+                                      from_lower,
+                                      from_upper)
+
+    # Get farther point from from_set to target_center
+    dist_lower = torch.abs(target_center[None, :, :] - from_lower[:, None, :])
+    dist_upper = torch.abs(target_center[None, :, :] - from_upper[:, None, :])
+    farther = torch.where(dist_lower > dist_upper, from_lower[:, None, :], from_upper[:, None, :])
+
     if supremum:
-        opt_means = torch.clamp(target_centers.unsqueeze(0), min=from_lower.unsqueeze(1), max=from_upper.unsqueeze(1))
+        optimal_means = projection
+        if target_is_partition:
+            optimal_means[:, -1, :] = farthest_from_shell
+
     else: # Compute inf
-        dist_lower = torch.abs(target_centers[None, :, :] - from_lower[:, None, :])
-        dist_upper = torch.abs(target_centers[None, :, :] - from_upper[:, None, :])
-        opt_means = torch.where(dist_lower > dist_upper, from_lower[:, None, :], from_upper[:, None, :])
+        optimal_means = farther
 
-    lower_std = (target_lower - opt_means) / std_devs
-    upper_std = (target_upper - opt_means) / std_devs
+    probs = kernel_probs_given_optimal_means(optimal_means, covariance, target_lower, target_upper, target_is_partition)
+    probs[-1, :] = 0.0 # R_unbounded as absorbing state
 
-    lower_std[lower_std.isnan()] = float('inf')
-    upper_std[upper_std.isnan()] = float('inf')
-
-    # Compute Gaussian probs for each pair (\barR_from, R_target)
-    lower_cdf = normal.cdf(lower_std)
-    upper_cdf = normal.cdf(upper_std)
-    probs_per_dimension = upper_cdf - lower_cdf
-    probs = probs_per_dimension.prod(dim=-1)
-
-    # Separate treatment for unbounded region
+    # Assuming unbounded region to be absorbing state
     if isinstance(target, HyperRectangularPartition):
-        if supremum:
-            probs[-1, :] = 0.01
-            probs[:, -1] = 0.01
-            probs[-1, -1] = 1.0
-        else:
-            probs[-1, :] = 0.0
-            probs[:, -1] = 0.0
+        probs[-1, -1] = 1.0
+
+    return probs
+
+
+def transition_kernel(f: Dynamics,
+                            locs: torch.tensor,
+                            target: Union[HyperRectangularPartition, HyperRectangle],
+                            covariance: torch.Tensor):
+    kernel_means = f(locs)
+    probs = gaussian_probabilities(kernel_means, covariance, target)
+    probs[-1, :] = 0.0 # R_unbounded as absorbing state
+
+    if isinstance(target, HyperRectangularPartition):
+        probs[-1, -1] = 1.0
 
     return probs
 
@@ -274,7 +297,13 @@ def compute_kernel_at_locs(f, locs, covariance, regions, set):
         prob_set = kernel.compute_probabilities(set)
         probs_set.append(prob_set)
 
-    return torch.stack(probs_regions), torch.tensor(probs_set)
+    probs_regions = torch.stack(probs_regions)
+    probs_regions[-1, :] = 0.0
+    probs_regions[-1, -1] = 1.0
+
+    probs_set = torch.tensor(probs_set)
+
+    return probs_regions, probs_set
 
 def o_maximization(coeffs:torch.Tensor, lower_bounds:torch.Tensor, upper_bounds:torch.Tensor):
     # inspired from https://www.baymler.com/IntervalMDP.jl/dev/algorithms/#Efficient-value-iteration
