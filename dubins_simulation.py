e54f715c2167bb9660efd4e5264d15d94ac641ba
adrienbanse <adrien.banse@gmail.com>
2025-04-16 16:41:21 +0200

    dubin's update, essentially clamp alphas and betas

--- dubins_simulation.py
+++ dubins_simulation.py
@@ -23,16 +23,16 @@ if __name__ == '__main__':
 
     # Define partition
     shell = torch.tensor([
-        [-4., -4., 0.],
-        [10., 10., 2 * torch.pi]
+        [-4., -4., -1.],
+        [10., 10., 10.]
     ])
     loc_shell = get_shell_loc(shell)
 
     # Define unsafe set
-    unsafe_set = HyperRectangle(torch.tensor([3.5, 2.0, - torch.inf]).unsqueeze(0), torch.tensor([4.5, 3.0, torch.inf]).unsqueeze(0))
+    unsafe_set = HyperRectangle(torch.tensor([3.0, 1.0, -torch.inf]).unsqueeze(0), torch.tensor([5.0, 4.0, torch.inf]).unsqueeze(0))
 
     # Define initial partition
-    inner_locs = uniform_grid(shell[0], shell[1], 10)
+    inner_locs = uniform_grid(shell[0], shell[1], 3)
     partition = HyperRectangularPartition(inner_locs, loc_shell, shell)
 
     # Compute initial distribution
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     # For plotting
     lbs, ts, aps, ubs = [], [], [], []
 
-    for t in range(20):
+    for t in range(10):
 
         # Compute alpha and beta for unsafe set
         inf_kernel_unsafe_set = bound_transition_kernel(f, partition, unsafe_set, cov_noise, supremum=False).squeeze()
@@ -58,9 +58,12 @@ if __name__ == '__main__':
 
         # Compute bounds for unsafe set
         p_min = o_maximization(- inf_kernel_unsafe_set, approx_probs + alphas_regions, approx_probs + betas_regions)
-        p_max = o_maximization(sup_kernel_unsafe_set, approx_probs + alphas_regions, approx_probs + betas_regions)
+        p_max = o_maximization(sup_kernel_unsafe_set, approx_probs + alphas_regions, approx_probs + betas_regions)       
+
         alpha_unsafe_set = torch.dot(inf_kernel_unsafe_set, p_min) - torch.dot(kernel_at_locs_unsafe_set, approx_probs)
+        alpha_unsafe_set = torch.clamp(alpha_unsafe_set, - approx_unsafe, 1 - approx_unsafe)
         beta_unsafe_set = torch.dot(sup_kernel_unsafe_set, p_max) - torch.dot(kernel_at_locs_unsafe_set, approx_probs)
+        beta_unsafe_set = torch.clamp(beta_unsafe_set, - approx_unsafe, 1 - approx_unsafe)
 
         # Define new partition
         new_inner_locs = uniform_grid(shell[0], shell[1], 3)
@@ -69,28 +72,27 @@ if __name__ == '__main__':
 
         # print(f'Locs before refinement for t={t}: {new_partition.locs}')
 
-        for refinement in range(2):
+        for refinement in range(3):
             new_partition = refined_partition
+            new_approx_probs = approx_distribution.compute_probabilities(new_partition)
+
             inf_kernel_regions = bound_transition_kernel(f, partition, new_partition, cov_noise, supremum=False)
             sup_kernel_regions = bound_transition_kernel(f, partition, new_partition, cov_noise, supremum=True)
             kernel_at_locs_regions = transition_kernel(f, partition.locs, new_partition, cov_noise)
 
-            print(inf_kernel_regions[inf_kernel_regions > kernel_at_locs_regions])
-            print(kernel_at_locs_regions[inf_kernel_regions > kernel_at_locs_regions])
-            print()
-
-            print(sup_kernel_regions[sup_kernel_regions < kernel_at_locs_regions])
-            print(kernel_at_locs_regions[sup_kernel_regions < kernel_at_locs_regions])
-            print()
-
             next_alphas_regions, next_betas_regions = torch.zeros(len(new_partition.locs)), torch.zeros(len(new_partition.locs))
+
             for i, (inf_kernel_region, sup_kernel_region) in enumerate(zip(inf_kernel_regions.T, sup_kernel_regions.T)):
                 p_min = o_maximization(- inf_kernel_region, approx_probs + alphas_regions, approx_probs + betas_regions)
                 p_max = o_maximization(sup_kernel_region, approx_probs + alphas_regions, approx_probs + betas_regions)
                 next_alphas_regions[i] += torch.dot(inf_kernel_region, p_min)
                 next_betas_regions[i] += torch.dot(sup_kernel_region, p_max)
+
             next_alphas_regions -= kernel_at_locs_regions.T @ approx_probs
+            next_alphas_regions = torch.clamp(next_alphas_regions, - new_approx_probs, 1 - new_approx_probs)
+
             next_betas_regions -= kernel_at_locs_regions.T @ approx_probs
+            next_betas_regions = torch.clamp(next_betas_regions, - new_approx_probs, 1 - new_approx_probs)
 
             refined_partition = new_partition.refine(next_betas_regions, threshold=1e-3, pareto=1.0)
 
@@ -106,9 +108,11 @@ if __name__ == '__main__':
 
         # Update approximation
         partition = new_partition
-        approx_probs = approx_distribution.compute_probabilities(partition)
+        approx_probs = new_approx_probs
+
         approx_distribution = GaussianMixture(f(partition.locs), cov_noise, approx_probs)
 
     lbs, aps, ubs = torch.tensor(lbs), torch.tensor(aps), torch.tensor(ubs)
+    print(f'alphas for unsafe set: {lbs}')
     print(f'betas for unsafe set: {ubs}')
     plot_interval(aps, lbs, ubs)
