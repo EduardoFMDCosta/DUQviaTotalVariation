e2ef0bba3ae70f2ee93a346c5ed03727464bbd6c
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-05-15 19:23:24 +0200

    minor changes

--- main.py
+++ main.py
@@ -12,40 +12,44 @@ if __name__ == '__main__':
     # System parameters
     A = torch.Tensor(
         [
-            [0.9, 0.01],
-            [0.02, 0.5]
+            [0.84, 0.10],
+            [0.05, 0.72]
         ])
     f = LinearDynamics(A)
 
     # Initial distribution
-    mean_initial = torch.Tensor([2, 3])
-    cov_initial = 0.02 * torch.eye(2)
+    mean_initial = torch.Tensor([10, 7])
+    cov_initial = 0.005 * torch.eye(2)
     initial_distribution = Gaussian(mean_initial, cov_initial)
 
     # Noise distribution
     mean_noise = torch.Tensor([0, 0])
-    cov_noise = 0.1 * torch.eye(2)
+    cov_noise = 0.03 * torch.eye(2)
     noise_distribution = Gaussian(mean_noise, cov_noise)
 
     # Define unsafe set
-    unsafe_set = HyperRectangle(torch.tensor([3.0, 4]).unsqueeze(0), torch.tensor([5.0, 6]).unsqueeze(0))
+    unsafe_set = HyperRectangle(torch.tensor([4.0, 2.0]).unsqueeze(0), torch.tensor([5.0, 3.0]).unsqueeze(0))
+
+    # Set parameters
+    horizon = 10
+    num_samples = 1000
 
     mixtures, tv_bounds = propagate_mixture_tv_bounds(f=f,
                                                   initial_distribution=initial_distribution,
                                                   noise_distribution=noise_distribution,
-                                                  initial_grid_size=10,
-                                                  prediction_horizon=10,
-                                                  n_samples=1000)
+                                                  initial_grid_size=100,
+                                                  prediction_horizon=horizon,
+                                                  n_samples=num_samples)
 
     print(tv_bounds)
 
     monte_carlo_samples = monte_carlo(f=f,
                 initial_distribution=initial_distribution,
                 noise_distribution=noise_distribution,
-                prediction_horizon=10,
-                n_samples=1000)
+                prediction_horizon=horizon,
+                n_samples=num_samples)
 
     gmm_samples = sample_from_gmm(mixtures=mixtures,
-                    n_samples=1000)
+                    n_samples=num_samples)
 
     plot_samples(monte_carlo_samples, gmm_samples)
