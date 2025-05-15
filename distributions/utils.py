eba484ebd7e7d1565cd336e33e7cca4efc51f2f9
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-05-15 19:30:40 +0200

    change variable name

--- distributions/utils.py
+++ distributions/utils.py
@@ -1,21 +1,20 @@
 import torch
-from grid.regions import HyperRectangularPartition, HyperRectangle
 from typing import Union
-import torch.distributions as dist
+from grid.regions import HyperRectangularPartition, HyperRectangle
 
-def gaussian_probabilities(means: torch.Tensor,
+def gaussian_probabilities(mean: torch.Tensor,
                            covariance: torch.Tensor,
-                           regions: Union[HyperRectangularPartition, HyperRectangle]):
+                           regions: Union[HyperRectangularPartition, HyperRectangle, torch.Tensor]):
     lower = regions.lower
     upper = regions.upper
 
     sigma = torch.sqrt(torch.diag(covariance))
 
-    if means.dim() == 1:
-        means = means.unsqueeze(0)
+    if mean.dim() == 1:
+        mean = mean.unsqueeze(0)
 
-    lower_norm = (lower.unsqueeze(0) - means.unsqueeze(1)) / sigma
-    upper_norm = (upper.unsqueeze(0) - means.unsqueeze(1)) / sigma
+    lower_norm = (lower.unsqueeze(0) - mean.unsqueeze(1)) / sigma
+    upper_norm = (upper.unsqueeze(0) - mean.unsqueeze(1)) / sigma
 
     normal = torch.distributions.Normal(0.0, 1.0)
     lower_cdf = normal.cdf(lower_norm)
@@ -28,33 +27,5 @@ def gaussian_probabilities(means: torch.Tensor,
 
     if probs.shape[0] == 1:
         probs = probs.squeeze(0) # Squeeze it back to (n,) if means represent a sole Gaussian
-
-    return torch.clamp(probs, 0, 1)
-
-
-def kernel_probs_given_optimal_means(means_for_target: torch.Tensor,
-                                     covariance: torch.Tensor,
-                                     target_lower: torch.Tensor,
-                                     target_upper: torch.Tensor,
-                                     target_is_partition: bool):
-
-    normal = dist.Normal(0, 1)
-    std_devs = torch.sqrt(torch.diag(covariance))
-
-    lower_std = (target_lower - means_for_target) / std_devs
-    upper_std = (target_upper - means_for_target) / std_devs
-
-    lower_std[lower_std.isnan()] = - float('inf') # Q? Should always be negative?
-    upper_std[upper_std.isnan()] = float('inf')
-
-    # Compute Gaussian probs for each pair (\barR_from, R_target)
-    lower_cdf = normal.cdf(lower_std)
-    upper_cdf = normal.cdf(upper_std)
-
-    probs_per_dimension = upper_cdf - lower_cdf
-    probs = probs_per_dimension.prod(dim=-1)
-
-    if target_is_partition:
-        probs[:, -1] = 1 - probs[:, -1] # As we had computed P(shell)
 
     return probs
