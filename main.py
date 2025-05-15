9390228b181ed36296e9460dd4f222911692b5d3
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2025-05-15 17:10:27 +0200

    refactor main for tv experiments

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import torch
-from copy import deepcopy
-from dynamics import LinearDynamics, SinusoidalDynamics
-from distributions import Gaussian, GaussianMixture
+from dynamics import LinearDynamics
+from distributions import Gaussian
 from experiments import approximation_scheme_tv
-from optimization import gradient_descent
-from plotting import plot_interval
 from regions import HyperRectangle, HyperRectangularPartition
-from utils import compute_inf_sup_kernel, get_shell, get_shell_loc, uniform_grid, compute_kernel_at_locs, \
-    o_maximization, get_inf_sup_for_target_set, get_inf_sup_for_partition, bound_transition_kernel, transition_kernel
-
-import matplotlib.pyplot as plt
+from utils import get_shell_loc, uniform_grid
 
 if __name__ == '__main__':
     torch.manual_seed(0)
@@ -20,7 +14,6 @@ if __name__ == '__main__':
             [0.9]
         ])
     f = LinearDynamics(A)
-    #f = SinusoidalDynamics(1)
 
     # Initial distribution
     mean_initial = torch.Tensor([2])
@@ -45,81 +38,11 @@ if __name__ == '__main__':
     # Define unsafe set
     unsafe_set = HyperRectangle(torch.tensor([3.0]).unsqueeze(0), torch.tensor([5.0]).unsqueeze(0))
 
-    # TESTING NEW INF / SUP COMPUTATION
-    #inf_kernel_unsafe_set, sup_kernel_unsafe_set = get_inf_sup_for_target_set(f, cov_noise, partition, unsafe_set)
-    #inf_kernel_regions, sup_kernel_regions = get_inf_sup_for_partition(f, cov_noise, partition)
+    mixtures, tv_bounds = approximation_scheme_tv(f=f,
+                                                  initial_distribution=initial_distribution,
+                                                  noise_distribution=noise_distribution,
+                                                  initial_grid_size=10,
+                                                  prediction_horizon=2,
+                                                  n_samples=1000)
 
-    inf_kernel_unsafe_set = bound_transition_kernel(f, partition, unsafe_set, cov_noise, supremum=False).squeeze()
-    sup_kernel_unsafe_set = bound_transition_kernel(f, partition, unsafe_set, cov_noise, supremum=True).squeeze()
-    inf_kernel_regions = bound_transition_kernel(f, partition, partition, cov_noise, supremum=False)
-    sup_kernel_regions = bound_transition_kernel(f, partition, partition, cov_noise, supremum=True)
-
-    locs = partition.locs
-    kernel_at_locs_unsafe_set = transition_kernel(f, locs, unsafe_set, cov_noise).squeeze()
-    kernel_at_locs_regions = transition_kernel(f, locs, partition, cov_noise)
-
-    # Compute sup/inf_{z \in Ri} T(Rj | z) and sup/inf_{z \in Ri} T(U | z)
-    # inf_kernel_regions, sup_kernel_regions, inf_kernel_unsafe_set, sup_kernel_unsafe_set = compute_inf_sup_kernel(
-    #     f,
-    #     cov_noise,
-    #     partition,
-    #     unsafe_set
-    # )
-
-    # Initialize approximation distribution
-    approx_distribution = deepcopy(initial_distribution)
-    alphas_regions, betas_regions = torch.zeros(len(locs)), torch.zeros(len(locs))
-    alpha_unsafe_set, beta_unsafe_set = torch.zeros(1), torch.zeros(1)
-    
-    # For plotting
-    lbs, ts, aps, ubs = [], [], [], []
-
-    #Run simulation
-    mean_k = deepcopy(mean_initial)
-    cov_k = deepcopy(cov_initial)
-    means_gmm = f(locs)
-    for t in range(100):
-        # Compute the true distribution for comparaison
-        mean_k = torch.matmul(A, mean_k) + mean_noise
-        cov_k = torch.matmul(A, cov_k)
-        cov_k = torch.matmul(cov_k, torch.t(A)) + cov_noise
-        dis_k = Gaussian(mean_k, cov_k)
-
-        # Update approximation
-        approx_probs = approx_distribution.compute_probabilities(partition)
-        covs_noise = cov_noise
-        approx_distribution = GaussianMixture(means_gmm, covs_noise, approx_probs)
-
-        # Compute true error
-        true_unsafe = dis_k.compute_probabilities(unsafe_set)
-        approx_unsafe = approx_distribution.compute_probabilities(unsafe_set)
-        diff_unsafe = true_unsafe - approx_unsafe
-
-        # Compute bounds
-        p_min = o_maximization(- inf_kernel_unsafe_set, approx_probs + alphas_regions, approx_probs + betas_regions)
-        p_max = o_maximization(sup_kernel_unsafe_set, approx_probs + alphas_regions, approx_probs + betas_regions)
-
-        alpha_unsafe_set = torch.dot(inf_kernel_unsafe_set, p_min) - torch.dot(kernel_at_locs_unsafe_set, approx_probs)
-        beta_unsafe_set = torch.dot(sup_kernel_unsafe_set, p_max) - torch.dot(kernel_at_locs_unsafe_set, approx_probs)
-
-        next_alphas_regions, next_betas_regions = torch.zeros(len(locs)), torch.zeros(len(locs))
-        for i, (inf_kernel_region, sup_kernel_region) in enumerate(zip(inf_kernel_regions.T, sup_kernel_regions.T)):
-            p_min = o_maximization(- inf_kernel_region, approx_probs + alphas_regions, approx_probs + betas_regions)
-            p_max = o_maximization(sup_kernel_region, approx_probs + alphas_regions, approx_probs + betas_regions)
-            next_alphas_regions[i] += torch.dot(inf_kernel_region, p_min)
-            next_betas_regions[i] += torch.dot(sup_kernel_region, p_max)
-        next_alphas_regions -= kernel_at_locs_regions.T @ approx_probs
-        next_betas_regions -= kernel_at_locs_regions.T @ approx_probs
-        alphas_regions = next_alphas_regions
-        betas_regions = next_betas_regions
-
-        lbs.append(alpha_unsafe_set)
-        ts.append(true_unsafe)
-        aps.append(approx_unsafe)
-        ubs.append(beta_unsafe_set)
-
-        print("(t = {}) alpha = {}, true = {}, beta = {}".format(t, alpha_unsafe_set, diff_unsafe[0], beta_unsafe_set))
-        print(betas_regions.max())
-
-lbs, ts, aps, ubs = torch.tensor(lbs), torch.tensor(ts), torch.tensor(aps), torch.tensor(ubs)
-plot_interval(aps, lbs, ubs, ts)
+    print(tv_bounds)
